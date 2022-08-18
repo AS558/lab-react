eedc3066b55ef9269e76691dc72a3d44600b3fd7
AS558 <6310110558@psu.ac.th>
2022-08-18 23:17:36 +0700

    part7.1

--- src/WordCard.js
+++ src/WordCard.js
@@ -18,6 +18,7 @@ const prepareStateFromWord = given_word => {
 export default function WordCard(props){
 
     const [state, setState] = useState(prepareStateFromWord(props.value))
+    const [result, setResults] = useState("");
 
     const activationHandler = c => {
         console.log(`${c} has been activated.`)
@@ -29,19 +30,28 @@ export default function WordCard(props){
             if(guess == state.word){
                 console.log('yeah!')
                 setState({...state, guess: '', completed: true})
+                setResults("Correct");
             }else{
                 console.log('reset')
                 setState({...state, guess: '', attempt: state.attempt + 1})
+                setResults("Incorrect");
             }
         }
     }
 
     return (
-        <div>
-            {
-                state.chars.map((c, i) => 
-                <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>)
-            }
-        </div>
+        <>
+            {result != "" && (
+                <>
+                    <div>{result}</div>
+                </>
+            )}
+            <div>
+                {
+                    state.chars.map((c, i) => 
+                    <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>)
+                }
+            </div>
+        </>
     );
 }
