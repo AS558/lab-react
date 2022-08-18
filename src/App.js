7ec1d5406593953ff446f32388f01400f9b6fdc7
AS558 <6310110558@psu.ac.th>
2022-08-19 01:22:41 +0700

    part7.5

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word = "3SA03React";
 function App() {
   return ( 
     <div>
+      <div className='title'>WORD PUZZLE</div>
       <WordCard value={word}/>
     </div>
   );
